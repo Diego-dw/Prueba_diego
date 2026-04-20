0b2a09062f6e57a82961496f9f49a4a0a75b1941
Diego Armando Acosta Luna <diegoarmandoaluna@gmail.com>
2026-04-19 22:15:19 -0600

    se crean todos los getters

--- DiegoTry.py
+++ DiegoTry.py
@@ -6,6 +6,20 @@ class Trabajador:
     self.__altura = altura
     self.__salario = salario
 
-
+#getters
 def get_nombre(self):
     return self.__nombre
+
+def get_edad(self):
+    return self.__edad
+
+def get_peso(self):
+    return self.__peso
+
+def get_altura(self):
+    return self.__altura
+
+def get_salario(self):
+    return self.__salario
+
+#Setters
